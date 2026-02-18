3e209eea8f77f48b96e14255d62f941737154fdb
Christian Gajardo <c.gajardo08@ufromail.cl>
2026-02-18 12:18:34 -0300

    feat: Probando form

--- ai_agent/static/src/js/message_path.js
+++ ai_agent/static/src/js/message_path.js
@@ -12,17 +12,35 @@ export class MessagePath extends Component {
     static components = { ChatForm }; // Deben estar registrados aquí también
 
     static template = xml`
-        <div class="o_ai_chat_thread p-3">
-            <div t-foreach="state.messages" t-as="msg" t-key="msg.id" class="mb-3">
-                <t t-if="msg.type === 'component'">
-                    <t t-component="msg.component" t-props="msg.props"/>
-                </t>
-                <t t-else="">
-                    <div class="p-3 bg-light rounded border">
-                        <strong class="text-primary d-block">Asistente:</strong>
-                        <span t-esc="msg.body"/>
-                    </div>
-                </t>
+        <div class="o_ai_chat_container d-flex flex-column h-100">
+            <div class="o_ai_chat_thread p-3 flex-grow-1 overflow-auto">
+                <div t-foreach="state.messages" t-as="msg" t-key="msg.id" class="mb-3">
+                    <t t-if="msg.type === 'component'">
+                        <div class="agent-component-wrapper border rounded p-2 bg-white">
+                            <t t-component="msg.component" t-props="msg.props"/>
+                        </div>
+                    </t>
+                    <t t-else="">
+                        <div class="p-3 bg-light rounded border">
+                            <strong class="text-primary d-block">Asistente:</strong>
+                            <span t-esc="msg.body"/>
+                        </div>
+                    </t>
+                </div>
+            </div>
+
+            <div class="o_ai_input_area p-3 border-top bg-white">
+                <div class="input-group">
+                    <input type="text" 
+                        class="form-control" 
+                        placeholder="Escribe un mensaje..."
+                        t-model="state.currentInput"
+                        t-on-keydown="(ev) => ev.key === 'Enter' &amp;&amp; this.onSendMessage(state.currentInput)"/>
+                    <button class="btn btn-primary" 
+                        t-on-click="() => this.onSendMessage(state.currentInput)">
+                        Enviar
+                    </button>
+                </div>
             </div>
         </div>
     `;
