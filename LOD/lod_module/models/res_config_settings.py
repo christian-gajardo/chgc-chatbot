4b993787d0faa594308a85458453cfbf33ba7386
Christian Gajardo <c.gajardo08@ufromail.cl>
2026-02-19 11:09:07 -0300

    agent selector

--- LOD/lod_module/models/res_config_settings.py
+++ LOD/lod_module/models/res_config_settings.py
@@ -5,8 +5,15 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel): # heredamos de res.config.settings
     _inherit = 'res.config.settings' # heredamos de res.config.settings
 
-    chatbot_agent_id = fields.Many2one(
+    construction_api_key = fields.Char( # campos de configuracion
+        string='Gemini API Key', # string es el nombre que se muestra en el formulario
+        config_parameter='construction_materials.api_key', # config_parameter es el nombre que le damos a la variable
+        help='API Key de Google Gemini para el asistente IA' # help es la ayuda que se muestra en el formulario
+    )
+
+    construction_agent_id = fields.Many2one(
         'ai.agent',
-        string="Chatbot AI Agent",
-        config_parameter='construction_materials.chatbot_agent_id'
+        string='AI Agent',
+        config_parameter='construction_materials.agent_id',
+        help='Select which Odoo AI Agent will handle the assistant'
     )
